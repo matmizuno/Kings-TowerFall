b2d822a6a1e960f96cfb3f845ca394895ba43249
K1Melo <kauafernandes.smelo@gmail.com>
2024-02-20 00:19:29 -0300

    feat: intro animation finished

--- script/main.js
+++ script/main.js
@@ -10,7 +10,7 @@ let tl = gsap.timeline({
 ScrollTrigger.create({
   trigger:".imageWapper",
   start:"top top",
-  end:"+=2800",
+  end:"+=2000",
   animation:tl,
   scrub:true,
   pin:true
@@ -21,7 +21,9 @@ let openingText = document.querySelector(".openingText")
 let sloganText = document.querySelector(".sloganText")
 
 window.addEventListener("scroll", () => {
-  if(window.scrollY > (window.innerHeight * 0.5)) { logo.style.opacity = "0" } else if(window.scrollY < (window.innerHeight * 0.5)) { logo.style.opacity = "1" }
-  if(window.scrollY > (window.innerHeight * 0.8)) { logo.style.display = "none"; openingText.style.display = "inline"; } else {  logo.style.display = "block"; openingText.style.display = "none" }
-  if(window.scrollY > (window.innerHeight * 1.1)) { openingText.style.opacity = "0" } else { openingText.style.opacity = "1" }
+  if(window.scrollY > (window.innerHeight * 0.5)) { logo.style.opacity = "0"; logo.style.display = "block"; } else { logo.style.opacity = "1" }
+  if(window.scrollY > (window.innerHeight * 0.8)) { logo.style.display = "none"; openingText.style.display = "inline"; } else { openingText.style.display = "none" }
+  if(window.scrollY > (window.innerHeight * 1.1)) { openingText.style.opacity = "1"; } else { openingText.style.opacity = "0" }
+  if(window.scrollY > (window.innerHeight * 1.4)) { openingText.style.opacity = "0"; sloganText.style.display = "inline"; } else { sloganText.style.display = "none" }
+  if(window.scrollY > (window.innerHeight * 1.7)) { sloganText.style.opacity = "1"; } else { sloganText.style.opacity = "0" }
 })
