0052859160e7d398d0e7d42d593e43a58638a1a1
K1Melo <kauafernandes.smelo@gmail.com>
2024-02-20 17:48:09 -0300

    feat: animation issue solution

--- script/main.js
+++ script/main.js
@@ -1,29 +1,25 @@
-let tl = gsap.timeline({
-  delay: 0
-})
-  .to("#initial", 
-    {
-      width:"50%", 
-      opacity:0.7
-    })
+var tl = gsap.timeline();
+
+var tween = gsap.fromTo("#initial", { width: "100%", opacity: 1 }, { width: "50%", opacity: 0.5 });
+tl.add(tween);
 
 ScrollTrigger.create({
   trigger:".imageWapper",
   start:"top top",
-  end:"+=2000",
+  end:"+=2500",
   animation:tl,
   scrub:true,
   pin:true
 })
 
-let logo = document.getElementById("logoGameSphere");
-let openingText = document.querySelector(".openingText")
-let sloganText = document.querySelector(".sloganText")
+var logo = document.getElementById("logoGameSphere");
+var openingText = document.querySelector(".openingText")
+var sloganText = document.querySelector(".sloganText")
 
 window.addEventListener("scroll", () => {
   if(window.scrollY > (window.innerHeight * 0.5)) { logo.style.opacity = "0"; logo.style.display = "block"; } else { logo.style.opacity = "1" }
   if(window.scrollY > (window.innerHeight * 0.8)) { logo.style.display = "none"; openingText.style.display = "inline"; } else { openingText.style.display = "none" }
   if(window.scrollY > (window.innerHeight * 1.1)) { openingText.style.opacity = "1"; } else { openingText.style.opacity = "0" }
-  if(window.scrollY > (window.innerHeight * 1.4)) { openingText.style.opacity = "0"; sloganText.style.display = "inline"; } else { sloganText.style.display = "none" }
+  if(window.scrollY > (window.innerHeight * 1.4)) { openingText.style.opacity = "0"; sloganText.style.display = "inline"} else { sloganText.style.display = "none";}
   if(window.scrollY > (window.innerHeight * 1.7)) { sloganText.style.opacity = "1"; } else { sloganText.style.opacity = "0" }
 })
