e2f20d792caef4d8628b8d4a365a4dcda643efe4
K1Melo <kauafernandes.smelo@gmail.com>
2024-02-20 22:47:00 -0300

    feat: page and animation

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 var tl = gsap.timeline();
 
-var tween = gsap.fromTo("#initial", { width: "100%", opacity: 1 }, { width: "50%", opacity: 0.5 });
+var tween = gsap.fromTo("#initial", { height: "100%", width: "100%", opacity: 1 }, { height: "60%",  width: "60%",opacity: 0.5, delay: 1 });
 tl.add(tween);
 
 ScrollTrigger.create({
@@ -20,6 +20,6 @@ window.addEventListener("scroll", () => {
   if(window.scrollY > (window.innerHeight * 0.5)) { logo.style.opacity = "0"; logo.style.display = "block"; } else { logo.style.opacity = "1" }
   if(window.scrollY > (window.innerHeight * 0.8)) { logo.style.display = "none"; openingText.style.display = "inline"; } else { openingText.style.display = "none" }
   if(window.scrollY > (window.innerHeight * 1.1)) { openingText.style.opacity = "1"; } else { openingText.style.opacity = "0" }
-  if(window.scrollY > (window.innerHeight * 1.4)) { openingText.style.opacity = "0"; sloganText.style.display = "inline"} else { sloganText.style.display = "none";}
-  if(window.scrollY > (window.innerHeight * 1.7)) { sloganText.style.opacity = "1"; } else { sloganText.style.opacity = "0" }
+  if(window.scrollY > (window.innerHeight * 1.8)) { openingText.style.opacity = "0"; sloganText.style.display = "inline"} else { sloganText.style.display = "none";}
+  if(window.scrollY > (window.innerHeight * 2.2)) { sloganText.style.opacity = "1"; } else { sloganText.style.opacity = "0" }
 })
